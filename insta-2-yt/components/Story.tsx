09769d7bd6df0bdac36972a213b959418d340d70
Gourav Singh <87015615+Uglycrown@users.noreply.github.com>
2022-05-06 11:58:51 +0530

    hey

---
+++ insta-2-yt/components/Story.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Story = ({img, username}) => {
+  return (
+    <div>
+<img className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-125 duration-200 ease-in-out ' src={img} alt="" />
+<p className='text-xs w-14 truncate'>{username}</p>
+    </div>
+  )
+}
+
+export default Story
